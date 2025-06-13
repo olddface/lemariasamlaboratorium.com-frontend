98bddaf2256ae56aab18e548ce36b078b120fd20
olddfacem <alangxyz123@gmail.com>
2025-06-13 14:45:44 +0700

    new commit

--- README.md
+++ README.md
@@ -3,7 +3,9 @@ homepage = home.html
 about us = about-us.html
 contact us = contact.html
 product category = category.html
+detail produk = detail-produk.html
+
 
 <!-- COMPONENTS -->
-header = index.html
-footer = index.html
+header = home.html
+footer = home.html
--- js/main.js
+++ js/main.js
@@ -32,23 +32,6 @@ $(window).on('load', function() {
         }, 500);
         return false;
     });
-    $(window).on('scroll', function(){
-        var cur_pos = $(this).scrollTop();
-
-        sections.each(function(){
-            var top = $(this).offset().top - nav_height,
-                bottom = top + $(this).outerHeight();
-
-            if(cur_pos >= top && cur_pos <= bottom) {
-                nav.find('a').removeClass('active');
-                sections.removeClass('active');
-
-                $(this).addClass('active');
-                nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
-            }
-        });
-    });
-
 
     // PORTFOLIO ISOTOPE
     if($('.portfolio-items').length) {
